bf413d7da6564d7c8cc2851dea577e1c23e6c49e
Annaya Singh <singhannaya02@gmail.com>
2026-02-12 14:50:02 +0530

    Add files via upload

---
+++ SplashScreen.java
@@ -0,0 +1,56 @@
+package com.example.chatbox;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView logo;
+    TextView name,own1;
+
+    Animation topAnim, bottomAnim;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        logo = findViewById(R.id.logoimg);
+        name = findViewById(R.id.logonameimg);
+        own1 = findViewById(R.id.ownone);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        logo.setAnimation(topAnim);
+        name.setAnimation(bottomAnim);
+        own1.setAnimation(bottomAnim);
+
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent(SplashActivity.this, loginActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        },4000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,loginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
